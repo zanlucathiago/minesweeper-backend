320cbb655bd3a0ec62937eb5247658a68bdfd6d8
Thiago Monteiro Zanluca <zanlucathiago@gmail.com>
2020-02-03 22:03:43 -0300

    Correções de acesso ao banco

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+// const MongoClient = require('mongodb').MongoClient;
+// const uri =
+//   'mongodb+srv://zanlucathiago:Mkbm@@1401@minesweeper-epgan.gcp.mongodb.net/test?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// connect = (callback) => {
+//   client.connect((err) => {
+//     if ()
+//     const collection = client.db('test').collection('devices');
+//     // perform actions on the collection object
+//     client.close();
+//   });
+// };
+// module.exports = connect;
